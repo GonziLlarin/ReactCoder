0aaae9b3ef26a0f9026aaeeaee415be8f8c414b0
Gonzu <gonziillarin@gmail.com>
2022-11-04 15:02:43 -0300

    gg

--- src/componentes/ItemDetail/ItemDetail.js
+++ src/componentes/ItemDetail/ItemDetail.js
@@ -3,37 +3,28 @@ import ItemCount from '../ItemCount/ItemCount'
 import { useContext } from 'react'
 import { CartContext } from '../../context/CartContext'
 
-
-
 const ItemDetail = ({ id, name, img, description, price, stock}) => {
     
     const { addItem } = useContext(CartContext)
 
 
     const handleOnAdd = (quantity, stock) => {
-        const productToAdd = {
-            id, name, price,quantity
-        }
+        const productToAdd = {id, name, price,quantity}
+        
         addItem(productToAdd)
     }
 
     return (
         <article className="ItemDetail">
             <header>
-                <h2>
-                    {name}
-                </h2>
+                <h2>{name}</h2>
             </header>  
             <picture>
                 <img src={img} alt={name} className="ItemImg"/>
             </picture>
             <section>
-                <p>
-                    Descripción: {description}
-                </p>
-                <p>
-                    Precio: {price}
-                </p>
+                <p>Descripción: {description}</p>
+                <p>Precio: {price}</p>
             </section>           
             <footer className='ItemFooter'>
                 <ItemCount onAdd={handleOnAdd} stock={stock} />
--- src/componentes/ItemDetailContainer/ItemDetailContainer.js
+++ src/componentes/ItemDetailContainer/ItemDetailContainer.js
@@ -1,6 +1,5 @@
 import './ItemDetailContainer.css'
 import { useEffect, useState } from "react"
-// import { getProductById} from "../../asyncMock"
 import ItemDetail from "../ItemDetail/ItemDetail"
 import { useParams } from "react-router-dom"
 import { Link } from 'react-router-dom'
