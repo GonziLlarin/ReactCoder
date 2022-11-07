54f14874940c940240ecb8de876f71512c3ff536
Gonzu <gonziillarin@gmail.com>
2022-11-06 23:07:25 -0300

    VERCEL LCDTM

--- src/services/firebase/index.js
+++ src/services/firebase/index.js
@@ -2,10 +2,10 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from  'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_apiKey ,
-    authDomain: process.env.REACT_APP_authDomain ,
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
     projectId: process.env.REACT_APP_protectId,
-    storageBucket: process.env.REACT_APP_storageBucket ,
+    storageBucket: process.env.REACT_APP_storageBucket,
     messagingSenderId: process.env.REACT_APP_messagingSenderId,
     appId: process.env.REACT_APP_appId
 };
